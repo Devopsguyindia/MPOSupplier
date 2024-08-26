72cae6f02de9d9197a281f5c0d43a24ba62dff02
ahsan <ahsanahmadsn@gmail.com>
2024-08-26 21:12:26 +0500

    add profile

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MaterialModule } from 'src/shared/material/material.module';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
@@ -12,6 +12,7 @@ import { MaterialModule } from 'src/shared/material/material.module';
   declarations: [
     PagesComponent,
     MainLayoutComponent,
+    ProfileComponent,
   ],
   imports: [
     CommonModule,
