12c8892e9348d8d115fb9bc6f4c0749d5107a494
Ahsan Ahmad <ahsan.ahmad@ovada.com>
2024-08-26 11:55:04 +0500

    Update material

--- src/shared/material/material.module.ts
+++ src/shared/material/material.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatIconModule} from '@angular/material/icon'; 
+import {MatIconModule} from '@angular/material/icon';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 
@@ -12,12 +20,27 @@ import {MatIconModule} from '@angular/material/icon';
     CommonModule,
     MatButtonModule,
     MatDialogModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    MatExpansionModule,
+    MatDividerModule
+
   ],
   exports: [
     MatButtonModule,
     MatDialogModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    MatExpansionModule,
+    MatDividerModule
   ]
 })
 export class MaterialModule { }
