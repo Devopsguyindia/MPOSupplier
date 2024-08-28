7c83c90fafb4d62ee98c3b0cb6220c38e17756c7
ahsan <ahsanahmadsn@gmail.com>
2024-08-28 21:17:52 +0500

    Add contact UI

--- src/app/pages/profile/contact/contact.component.ts
+++ src/app/pages/profile/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatExpansionPanel } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  
+  @ViewChild('phonePanel') phonePanel: MatExpansionPanel;
+  @ViewChild('emailPanel') emailPanel: MatExpansionPanel;
+  @ViewChild('addressPanel') addressPanel: MatExpansionPanel;
+
+  phoneList = ['Phone 1', 'Phone 2', 'Phone 3', 'Phone 4'];
+  emailList = ['Email 1', 'Email 2', 'Email 3', 'Email 4'];
+  addressList = ['Address 1', 'Address 2', 'Address 3', 'Address 4'];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openPanel(panel: MatExpansionPanel) {
+    panel.open();
   }
 
 }
