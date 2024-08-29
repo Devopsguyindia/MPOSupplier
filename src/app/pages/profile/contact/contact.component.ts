a2eed3c42f2af1a3dff7ea03d05dfb6d896b747c
Ahsan Ahmad <ahsan.ahmad@ovada.com>
2024-08-29 18:26:52 +0500

    Add phone form

--- src/app/pages/profile/contact/contact.component.ts
+++ src/app/pages/profile/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MatExpansionPanel } from '@angular/material/expansion';
 
 @Component({
@@ -8,7 +9,7 @@ import { MatExpansionPanel } from '@angular/material/expansion';
 })
 export class ContactComponent implements OnInit {
 
-  
+
   @ViewChild('phonePanel') phonePanel: MatExpansionPanel;
   @ViewChild('emailPanel') emailPanel: MatExpansionPanel;
   @ViewChild('addressPanel') addressPanel: MatExpansionPanel;
@@ -17,9 +18,24 @@ export class ContactComponent implements OnInit {
   emailList = ['Email 1', 'Email 2', 'Email 3', 'Email 4'];
   addressList = ['Address 1', 'Address 2', 'Address 3', 'Address 4'];
 
+  phoneFormGroup!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.buildPhoneForm()
+  }
+
+  buildPhoneForm() {
+    this.phoneFormGroup = new FormGroup({
+      idcompany_contact_phone: new FormControl(),
+      phone_number: new FormControl(),
+      phone_type: new FormControl("2"),
+      usFormat: new FormControl(true),
+      isprimary: new FormControl(true),
+
+    });
   }
 
   openPanel(panel: MatExpansionPanel) {
